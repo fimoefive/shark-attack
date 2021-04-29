20ba4fcb6625af78a88185e7c1868e39aaf873a0
fimoefive <brothermj@hotmail.com>
2021-04-29 18:48:31 -0500

    made a LiveStudent card function in the LiveStudents.js file.

--- src/App/LiveStudent.js
+++ src/App/LiveStudent.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card, CardBody,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
-function LiveStudent() {
-
+function LiveStudent({ firstName, lastName }) {
+  <div>
+    <Card className="student-card">
+      <CardBody>
+        <CardTitle tag="h4">${firstName} {lastName}</CardTitle>
+        <CardSubtitle tag="h5" className="mb-2 text-muted">Student of Sharks</CardSubtitle>
+      </CardBody>
+    </Card>
+  </div>;
 }
+
+LiveStudent.prototypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+};
 
 export default LiveStudent;
