1dab2240e6b252a96ab4239f9816c89c75d7028c
fimoefive <brothermj@hotmail.com>
2021-04-29 19:27:46 -0500

    added the .map and liveStudents props for the SharkTank propTypes and SharkTank function.

--- src/App/GraveStone.js
+++ src/App/GraveStone.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card, CardBody,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
 
-function GraveStone() {
-
+function GraveStone({ firstName, lastName }) {
+  <div>
+    <Card className="student-card">
+      <CardBody>
+        <CardTitle tag="h4">{firstName} {lastName}</CardTitle>
+        <CardSubtitle tag="h5" className="mb-2 text-muted">Tomb Stone</CardSubtitle>
+      </CardBody>
+    </Card>
+  </div>;
 }
+
+GraveStone.prototypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+};
 
 export default GraveStone;
