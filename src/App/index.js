1dca57a604da4cd29d4c61155f2bf15ac81ca97b
fimoefive <brothermj@hotmail.com>
2021-05-11 16:46:35 -0500

    added RESET Button in the return statement of the App function and also imported reset form studentsData.js file.

--- src/App/index.js
+++ src/App/index.js
@@ -3,7 +3,8 @@ import { Button } from 'reactstrap';
 import {
   livingStudents,
   dearlyBeloved,
-  followTheLight
+  followTheLight,
+  reset
 } from '../helpers/data/studentsData';
 import StudentList from '../components/StudentList';
 import './App.scss';
@@ -31,9 +32,11 @@ function App() {
       <Button color="danger" onClick={attackButton}
         disabled={allStudents.length <= 0}
       >Shark Bite</Button>
+      <Button onClick{...() => attackButton(reset)}>RESET</Button>
 
       <h3>Shark Tank</h3>
       <StudentList color='info' studentArray={allStudents} />
+
       <h3>Graveyard</h3>
       <StudentList color='secondary' studentArray={deadStudents} />
     </div>
